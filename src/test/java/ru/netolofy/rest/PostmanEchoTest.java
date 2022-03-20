ff0cf6fb9c5e25c07482302b9c4b6770d4f26d33
Maksim Ivlev <brainlucker@gmail.com>
2022-03-20 19:54:02 +0300

    Revert "Status check with broken test"
    This reverts commit b54c7ee9480f74ed7d20fb9d3523456c5b4396ce.

--- src/test/java/ru/netolofy/rest/PostmanEchoTest.java
+++ src/test/java/ru/netolofy/rest/PostmanEchoTest.java
@@ -22,7 +22,7 @@ class PostmanEchoTest {
       // Проверки
       .then()
           .statusCode(200)
-          .body("data", equalTo("Текст"))
+          .body("data", equalTo("Тест"))
       ;
     }
 }
